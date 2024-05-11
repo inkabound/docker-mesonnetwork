d0d85f132e739f1f057e65a327d584f43f929270
inkabound <158990409+inkabound@users.noreply.github.com>
2024-05-12 01:09:35 +0700

    Update dockersetup.sh

--- dockersetup.sh
+++ dockersetup.sh
@@ -6,3 +6,4 @@ rm -f "${tar}.tar.gz"
 cd ./${tar}/ && sudo ./service install meson_cdn
 sudo ./meson_cdn config set --token="${token}" --https_port="${https_port}" --cache.size="${cache_size}"
 sudo ./meson_cdn
+sudo ./service restart meson_cdn
